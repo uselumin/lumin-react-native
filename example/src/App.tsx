452ddd03fde0dbadffc6de26b95fa426902dc257
Jonas-Merlin Schumacher <jonas.merlin.schumacher@mailbox.org>
2023-04-26 13:02:18 +0200

    chore: add button to test .trackCustomEvent method to example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,16 +1,25 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-// import { Lumin } from '@lumin/react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 
-// const lumin = new Lumin('', 'dev');
+import { Lumin } from '@uselumin/react-native';
 
-// lumin.logInfo();
+const lumin = new Lumin('clgxl0fc200013t6cbmw74m9b:clgxl0fc200003t6cub9gplzn', {
+  url: 'http://localhost:3000',
+});
+
+lumin.init();
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Test</Text>
+      <Button
+        title="Track Event"
+        onPress={() => {
+          lumin.trackCustomEvent('test_event');
+        }}
+      />
     </View>
   );
 }
